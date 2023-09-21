d0d3a64c1ef54d20ceae85a1732e8fdf3ec3135d
Dino <cdmx1814@amerike.edu.mx>
2023-09-21 15:57:42 -0600

    Enemy core

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreController.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreController.cs
@@ -2,21 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using com.LazyGames;
+using Lean.Pool;
+
 public class EnemyCoreController : MonoBehaviour
 {
     #region Serialized Fields
+
     [SerializeField] private EnemyCoreData enemyCoreData;
     [SerializeField] private Transform[] spawnPoints;
+
     #endregion
+
+    #region private variables
+
+    private int _currentHealth;
+
+
+
+    #endregion
+
+    #region unity methods
+
+
 
     void Start()
     {
-        
+        _currentHealth = enemyCoreData.MaxHealth;
+        SpawnEnemyWave();
     }
 
     void Update()
     {
+
+    }
+
+    #endregion
+
+    #region private methods
+
+
+
+    private void ReceiveDamage(int damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            Debug.Log("Enemy Core Destroyed");
+        }
+    }
+
+    private void SpawnEnemyWave()
+    {
+        var enemy = LeanPool.Spawn(enemyCoreData.EnemyPrefab);
+        enemy.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
         
     }
-    
+
+#endregion
+
 }
+
+
+
--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreData.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreData.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace com.LazyGames
 {
 [CreateAssetMenu(fileName = "EnemyCoreData", menuName = "LazySheeps/Enemy Core", order = 1)]
     public class EnemyCoreData : ScriptableObject
     {
-        public int health = 1000;
-        public int enemyPerWave = 10;
-        public float spawnDelay = 0.5f;
-        
+        #region Serialized Fields
+        [SerializeField] private int maxHealth = 1000;
+        [SerializeField] private int enemyPerWave = 10;
+        [SerializeField] private float spawnDelay = 0.5f;
         [SerializeField] private float waveDelay = 5f;
         [SerializeField] private GameObject enemyPrefab;
-
+        #endregion
+        
+        #region public variables
         public GameObject EnemyPrefab => enemyPrefab; 
         public float WaveDelay => waveDelay;
-
+        public int EnemyPerWave => enemyPerWave;
+        public float SpawnDelay => spawnDelay;
+        public int MaxHealth => maxHealth;
+        #endregion
     }
 }
