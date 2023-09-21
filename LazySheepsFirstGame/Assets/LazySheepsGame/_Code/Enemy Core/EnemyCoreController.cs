3c4e1033ffd8356be985048635c10ed84f3ee535
Dino <cdmx1814@amerike.edu.mx>
2023-09-21 15:22:51 -0600

    Lean Pool Plugin for enemies

---
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.LazyGames;
+public class EnemyCoreController : MonoBehaviour
+{
+    #region Serialized Fields
+    [SerializeField] private EnemyCoreData enemyCoreData;
+    [SerializeField] private Transform[] spawnPoints;
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
