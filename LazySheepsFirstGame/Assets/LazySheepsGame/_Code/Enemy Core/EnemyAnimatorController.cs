db21ebfd4227f7f01fdbb4765cbbfabd39080c4a
Sr Almkeja <cdmx1895@amerike.edu.mx>
2023-10-11 16:35:38 -0600

    enemyAnimatorController refactor

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyAnimatorController.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyAnimatorController.cs
@@ -2,25 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using com.LazyGames.DZ;
 
 namespace com.LazyGames.DZ
 {
    public class EnemyAnimatorController : MonoBehaviour
    {
-      #region Variables
+        #region Variables
+        [Header("Enemy Anim Configs")]
+        [SerializeField] List<EnemyAnimConfg> animConfgs = new List<EnemyAnimConfg>();
+        [SerializeField] private EnemyAnimConfg currentAnim;
 
-      [Header("Bolleans only Test Animayions")]
-      private bool _isIdle, _isWalk, _isAttack; 
-      [Header("Enemy Animator")]
-      [SerializeField] private Animator _animator;
+        [Header("Testing")]
+        public string TESTAnim;
+        public bool valueTestAnim;
 
 
-      #endregion
+
+        /*
+        [Header("Bolleans only Test Animations")]
+        [SerializeField] private bool _isIdle, _isWalk, _isAttack; */
+
+        [Header("Enemy Animator")]
+        [SerializeField] private Animator _animator;
+
+        public Action onChangeAnim;
 
 
-      private void Update()
+
+        #endregion
+
+
+        private void Start()
+        {
+            //SetAnim()
+            
+        }
+        private void Update()
       {
-         // Actualizar los booleanos en el Animator según las condiciones
+        /* // Actualizar los booleanos en el Animator según las condiciones
          _animator.SetBool("IsIdle", _isIdle);
          _animator.SetBool("IsWalk", _isWalk);
          _animator.SetBool("IsAttack", _isAttack);
@@ -29,9 +50,10 @@ namespace com.LazyGames.DZ
          {
             _isIdle = true;
             _animator.SetBool("IsIdle", true);
-         }
+         }*/
       }
-      
+     
+        /*
       public void SetIdle(bool value)
       {
          _isIdle = value;
@@ -46,9 +68,60 @@ namespace com.LazyGames.DZ
       {
          _isAttack = value;
       }
+        */
+        public void SetAnim(string name, bool active)
+        {
+            Debug.Log("Entra aquí");
+            EnemyAnimConfg config = null;
+            foreach (var anim in animConfgs)
+            {
+                if(anim.nameAnim == name)
+                {
+                    config = anim;
+
+                }
+            }
+            _animator.SetBool(config.nameParameter, active);
+            currentAnim = config;
+            onChangeAnim?.Invoke();
+        }
       
       //Para Activar cualquier animacion hacer lo siguiente -->
       //_enemyAnimator.SetIdle/.SetWalk/.SetAttack/.SetFutureAnimations(true) o false para apagar
       //Idle se activara de forma automatica si no hay nada activado
    }
 }
+[Serializable]
+public class EnemyAnimConfg
+{
+    public string nameAnim;
+    public string nameParameter;
+    public bool isActive;
+    
+    public void DoCrossfadeToAnim(string nextAnim)
+    {
+
+    }
+
+}
+
+
+ #if UNITY_EDITOR_WIN
+
+ [CustomEditor(typeof(EnemyAnimatorController))]
+ public class EnemyAnimatorControllerEditor : Editor
+{ 
+     public override void OnInspectorGUI()
+    {
+         DrawDefaultInspector();
+        EnemyAnimatorController controller = (EnemyAnimatorController)target;
+
+
+                if (GUILayout.Button("PlayAnim"))
+                {
+            controller.SetAnim(controller.TESTAnim, controller.valueTestAnim);
+                }
+
+    }
+}
+#endif
