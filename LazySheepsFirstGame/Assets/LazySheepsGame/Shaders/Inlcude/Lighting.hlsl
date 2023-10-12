bef9b6b6f1681fbdabc0b296d26053db338f44ea
Sr Almkeja <cdmx1895@amerike.edu.mx>
2023-10-12 16:33:21 -0600

    Toon Shader80%

--- LazySheepsFirstGame/Assets/LazySheepsGame/Shaders/Inlcude/Lighting.hlsl
+++ LazySheepsFirstGame/Assets/LazySheepsGame/Shaders/Inlcude/Lighting.hlsl
@@ -4,7 +4,7 @@
 void CalculateMainLight_float(float3 WorldPos,out float3 Direction, out float3 Color, out half DistanceAtten, out half ShadowAtten)
 {
     #if SHADERGRAPH_PREVIEW
-    Direction = normilize(half3(0.5,0.5,0));
+    Direction = normalize(float3(0.5f, 0.5f, 0));
     Color = 1;
     DistanceAtten = 1;
     ShadowAtten = 1;
@@ -15,9 +15,11 @@ void CalculateMainLight_float(float3 WorldPos,out float3 Direction, out float3 C
         #else
             half4 shadowcoord = TransformWorldToShadowCoord(WorldPos);
         #endif
-    Light mainLight = GetMainLight(0);
+    Light mainLight = GetMainLight(shadowcoord);
     Direction = mainLight.direction;
     Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
     #endif
 }
 #endif
