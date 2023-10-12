bde778d7a3947cf88734dc752838fc6ce39a527f
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-10-11 22:46:13 -0600

    fix enemy hit

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Targets/EnemyTarget/EnemyTarget.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Targets/EnemyTarget/EnemyTarget.cs
@@ -1,78 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using com.LazyGames;
+using com.LazyGames.DZ;
 using UnityEngine;
 
 namespace com.LazyGames
 {
-    public class EnemyTarget : MonoBehaviour, IGeneralTarget
+    public class EnemyTarget : MonoBehaviour
     {
+        #region public Methods
 
-        [Header("Enemy Target")] 
-        [SerializeField] private TargetsData targetsData;
+        [SerializeField] private EnemyAnimatorController animatorController;
 
-        private string id;
-        private int maxHealth;
-        private TargetsType type;
-        private float currentHp;
-
-        
-        #region public variables
-
-        public string ID
-        {
-            get => id;
-            set => id = value;
-        }
-
-        public int MaxHealth
-        {
-            get => maxHealth;
-            set => maxHealth = value;
-        }
-
-        public TargetsType Type
-        {
-            get => type;
-            set => type = value;
-        }
-
-        public float CurrentHp
-        {
-            get => currentHp;
-            set => currentHp = value;
-        }
-
-        #endregion
-
-        #region Unity methods
 
         private void Start()
         {
-            PrepareTarget();
+            
         }
-    
+
+        private void HandleHitPoint(Vector3 direction)
+        {
+            // handle which area of the body was hit
+
+            
+            
+            
+        }
+
+        private void SetBleedingEffect()
+        {
+            
+        }
+            
 
         #endregion
-    
-        #region private methods
         
-        private void PrepareTarget()
-        {
-            ID = targetsData.ID;
-            Type = targetsData.Type;
-            MaxHealth = targetsData.MaxHealth;
-            CurrentHp = MaxHealth;
-        }
-
-        #endregion
-
-        public void ReceiveAggression(Vector3 direction, float velocity, float dmg = 0)
-        {
-            Debug.Log($"received aggressor");
-        }
-
     }
 
+    public enum EnemyBodyPart
+    {
+        Head,
+        LeftSide,
+        RightSide,
+    }
 }
