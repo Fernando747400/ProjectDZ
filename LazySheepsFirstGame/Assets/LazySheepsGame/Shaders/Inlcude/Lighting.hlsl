0005a197cb8e14aad5f4e8d826b07cdaa7a42604
Sr Almkeja <cdmx1895@amerike.edu.mx>
2023-10-12 15:44:51 -0600

    ya

--- LazySheepsFirstGame/Assets/LazySheepsGame/Shaders/Inlcude/Lighting.hlsl
+++ LazySheepsFirstGame/Assets/LazySheepsGame/Shaders/Inlcude/Lighting.hlsl
@@ -4,7 +4,7 @@
 void CalculateMainLight_float(float3 WorldPos,out float3 Direction, out float3 Color, out half DistanceAtten, out half ShadowAtten)
 {
     #if SHADERGRAPH_PREVIEW
-    Direction = half3(0.5,0.5,0);
+    Direction = normilize(half3(0.5,0.5,0));
     Color = 1;
     DistanceAtten = 1;
     ShadowAtten = 1;
