f2980749951bb204906dc0ff3a7c3a5e5d1a1223
CryoStorage <iracoon2014@gmail.com>
2023-10-16 22:26:03 -0600

    Siege logic expanded
    - Extracted walls list to scriptable obj
    - GetClosestWall func
    - Updating walls list through build and destroy events

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/AI/RuntimeMeshBuilder.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/AI/RuntimeMeshBuilder.cs
@@ -4,49 +4,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.AI.Navigation;
 using UnityEngine.AI;
+using com.LazyGames.Dio;
 
-
-public class RuntimeMeshBuilder : MonoBehaviour
+namespace com.LazyGames.DZ
 {
-    [SerializeField] private NavMeshSurface surface;
-    [SerializeField] private float boundsSize = 100f;
-    [SerializeField] private GameObject target;
-    [SerializeField]private LayerMask layerMask;
-
-    private Vector3 _targetPos;
-    private Bounds _bounds;
-    private Vector3 _lastAreaPosition;
-    private NavMeshData _navData;
-    private List<NavMeshBuildSource> _buildSources;
-
-
-    private void Awake()
+    public class RuntimeMeshBuilder : MonoBehaviour
     {
-        Prepare();
-    }
-
-    private void Update()
-    {
-        if (!Input.GetKeyDown(KeyCode.Q)) return;
-        OnBuildWall();
-    }
-
-    private void OnBuildWall()
-    {
-        surface.BuildNavMesh();
-    }
-    private void Prepare()
-    {
-        Vector3 boundsArea = new Vector3(boundsSize, 10, boundsSize);
-        _bounds = new Bounds(target.transform.position, boundsArea);
-        surface.agentTypeID = NavMesh.GetSettingsByIndex(0).agentTypeID; // Set the agent type
-        surface.collectObjects = CollectObjects.Volume;
-        surface.useGeometry = NavMeshCollectGeometry.PhysicsColliders;
-        surface.layerMask = layerMask;
-        surface.center = _bounds.center;
-        surface.size = _bounds.size;
+        [SerializeField] private NavMeshSurface surface;
+        [SerializeField] private float boundsSize = 100f;
+        [SerializeField] private GameObject target;
+        [SerializeField] private LayerMask layerMask;
+        [SerializeField] private GameObjectEventChannelSO buildEventChannelSo;
+        [SerializeField] private GameObjectEventChannelSO destroyEventChannelSo;
         
-        surface.BuildNavMesh();
-    }
+        private Vector3 _targetPos;
+        private Bounds _bounds;
+        private Vector3 _lastAreaPosition;
+        private NavMeshData _navData;
+        private List<NavMeshBuildSource> _buildSources;
 
+
+        private void OnEnable()
+        {
+            buildEventChannelSo.GameObjectEvent += OnGeometryChanged;
+            destroyEventChannelSo.GameObjectEvent += OnGeometryChanged;
+        }
+
+        private void Awake()
+        {
+            Prepare();
+        }
+
+        private void OnGeometryChanged(GameObject wall)
+        {
+            surface.BuildNavMesh();
+        }
+        private void Prepare()
+        {
+            Vector3 boundsArea = new Vector3(boundsSize, 10, boundsSize);
+            _bounds = new Bounds(target.transform.position, boundsArea);
+            surface.agentTypeID = NavMesh.GetSettingsByIndex(0).agentTypeID; // Set the agent type
+            surface.collectObjects = CollectObjects.Volume;
+            surface.useGeometry = NavMeshCollectGeometry.PhysicsColliders;
+            surface.layerMask = layerMask;
+            surface.center = _bounds.center;
+            surface.size = _bounds.size;
+            
+            surface.BuildNavMesh();
+        }
+    }
 }
