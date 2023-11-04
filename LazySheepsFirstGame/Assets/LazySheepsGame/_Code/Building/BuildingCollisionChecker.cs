2b8edfd751c2375522a60364b90dbd5a69a168bb
Fernando747 <fernandojcr5@gmail.com>
2023-11-03 23:45:00 -0600

    Player build system change

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Building/BuildingCollisionChecker.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Building/BuildingCollisionChecker.cs
@@ -71,6 +71,7 @@ public class BuildingCollisionChecker : MonoBehaviour
         if (collision.gameObject.tag == "Hammer")
         {
             HammerCollisionEvent.RaiseEvent();
+            Debug.Log("Collision with hammer");
         }
     }
 
@@ -79,6 +80,7 @@ public class BuildingCollisionChecker : MonoBehaviour
         if (other.gameObject.tag == "Hammer")
         {
             HammerCollisionEvent.RaiseEvent();
+            Debug.Log("Trigger with hammer");
         }
     }
 
