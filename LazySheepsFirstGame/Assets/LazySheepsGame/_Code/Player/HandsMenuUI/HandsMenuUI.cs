04a0803859a696598644cc3a1de7ce0d8cf717b3
Fernando747 <fernandojcr5@gmail.com>
2023-11-04 04:22:32 -0600

    HandUI small fix

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Player/HandsMenuUI/HandsMenuUI.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Player/HandsMenuUI/HandsMenuUI.cs
@@ -6,14 +6,18 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using NaughtyAttributes;
 
 public class HandsMenuUI : MonoBehaviour
 {
     [SerializeField] private GameObject _handsMenuUI;
     [SerializeField] private Button _runButton;
     [SerializeField] private GenericDataEventChannelSO changeSceneChannel;
-    [SerializeField] private string sceneRun = "VerticalSlice_0.1";
-    [SerializeField] private string sceneTabern = "Tabern";
+
+    [Scene]
+    [SerializeField] private string sceneRun;
+    [Scene]
+    [SerializeField] private string sceneTabern;
     void Start()
     {
         
@@ -24,16 +28,12 @@ public class HandsMenuUI : MonoBehaviour
         PlayerManager.Instance.SelectWeapon(weaponData.ID);
     }
    
-
-   
     public void OnClickRun()
     {
         _runButton.gameObject.SetActive(false);
         PlayerManager.Instance.ResetPlayersPosition();
         changeSceneChannel.RaiseStringEvent(sceneRun);
-        changeSceneChannel.RaiseBoolEvent(true);
-        
-        
+        changeSceneChannel.RaiseBoolEvent(true);    
     }
     
     public void OnClickReturn()
@@ -41,8 +41,7 @@ public class HandsMenuUI : MonoBehaviour
         _runButton.gameObject.SetActive(true);
         PlayerManager.Instance.ResetPlayersPosition();
         changeSceneChannel.RaiseStringEvent(sceneTabern);
-        changeSceneChannel.RaiseBoolEvent(true);
-        
+        changeSceneChannel.RaiseBoolEvent(true);        
     }
     
 }
