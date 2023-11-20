9303923ea5ff5ac17faed5cfff1cc63f3c5f2004
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-11-19 22:45:27 -0600

    object in hand

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Currency/CurrencyManager.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Currency/CurrencyManager.cs
@@ -10,6 +10,7 @@ public class CurrencyManager : ManagerBase
     [Header("Currency")]
     [SerializeField] private AddCurrencyEventChannel addCurrencyEventChannel;
     [SerializeField] private RemoveCurrencyEventChannel removeCurrencyEventChannel;
+    [SerializeField] private IntEventChannelSO onUpdateCurrencyChannel;
     
     [SerializeField] private int initialCurrency = 5000;
     
@@ -36,8 +37,13 @@ public class CurrencyManager : ManagerBase
     public int CurrentCurrency
     {
         get { return _currentCurrency; }
-        
-        protected set { _currentCurrency = value; }
+
+        protected set
+        {
+            _currentCurrency = value;
+            onUpdateCurrencyChannel.RaiseEvent(_currentCurrency);
+            
+        }
     }
 
     
