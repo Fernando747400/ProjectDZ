0f355ccd90e3f5c90f1bfb1a8427698569a8f7b1
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-11-22 07:44:04 -0600

    ultimos fixes antes de la luz

--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreController.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Enemy Core/EnemyCoreController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using com.LazyGames;
 using UnityEngine;
 using com.LazyGames.Dio;
 using com.LazyGames.DZ;
--- LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Player/HandsMenuUI/HandsMenuUI.cs
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Player/HandsMenuUI/HandsMenuUI.cs
@@ -7,49 +7,43 @@ using TMPro;
 
 public class HandsMenuUI : MonoBehaviour
 {
-    
-    [Header("Player Manager")]
-    [SerializeField] private PlayerManager _playerManager;
+
+    [Header("Player Manager")] [SerializeField]
+    private PlayerManager _playerManager;
 
     [SerializeField] private WeaponStoreManager _weaponStoreManager;
-    
-    [Header("UI")]
-    [SerializeField] private GameObject _runButton;
+
+    [Header("UI")] [SerializeField] private GameObject _runButton;
     [SerializeField] private GameObject exitButton;
-    
-    [Header("Health")]
-    [SerializeField] private IntEventChannelSO onUpdatePlayerHealth;
+
+    [Header("Health")] [SerializeField] private IntEventChannelSO onUpdatePlayerHealth;
     [SerializeField] private TMP_Text _playerHealthText;
     [SerializeField] Slider _playerHealthSlider;
-    
-    [Header("Currency")]
-    [SerializeField] private IntEventChannelSO onUpdatePlayerCurrency;
+
+    [Header("Currency")] [SerializeField] private IntEventChannelSO onUpdatePlayerCurrency;
     [SerializeField] private TMP_Text _playerCurrencyText;
-    
-    [Header("Objective")]
-    [SerializeField] private TMP_Text _objectiveText;
+
+    [Header("Objective")] [SerializeField] private TMP_Text _objectiveText;
     [SerializeField] private GenericDataEventChannelSO onObjectiveCompletedChannel;
 
-    
-    [Header("Scenes Channel")]
-    [SerializeField] private GenericDataEventChannelSO _changeSceneChannel;
-    [Scene]
-    [SerializeField] private string _sceneRun;
-    [Scene]
-    [SerializeField] private string _sceneTabern;
-    [Scene]
-    [SerializeField] private string _sceneAI;
+
+    [Header("Scenes Channel")] [SerializeField]
+    private GenericDataEventChannelSO _changeSceneChannel;
+
+    [Scene] [SerializeField] private string _sceneRun;
+    [Scene] [SerializeField] private string _sceneTabern;
+    [Scene] [SerializeField] private string _sceneAI;
 
     void Start()
     {
         onUpdatePlayerHealth.IntEvent += UpdatePlayerHealth;
         onUpdatePlayerCurrency.IntEvent += UpdatePlayerCurrency;
-        
+
         exitButton.gameObject.SetActive(false);
         _runButton.gameObject.SetActive(false);
-        
-        if(_playerManager == null) _playerManager = PlayerManager.Instance;
-        
+
+        if (_playerManager == null) _playerManager = PlayerManager.Instance;
+
         _playerManager.OnSetObjective += SetObjective;
         SetObjective(_playerManager.CurrentObjective);
         onObjectiveCompletedChannel.StringEvent += OnCompletedObjective;
@@ -58,8 +52,15 @@ public class HandsMenuUI : MonoBehaviour
         SetLifeValue(_playerManager.CurrentHealth);
 
     }
-    
-    public void OnClickWeapon(WeaponData weaponData)
+
+
+    public void GetEvent()
+    {
+        onObjectiveCompletedChannel.StringEvent += OnCompletedObjective;
+
+    }
+
+public void OnClickWeapon(WeaponData weaponData)
     {
         PlayerManager.Instance.SelectWeaponPlayerHolster(weaponData.ID);
     }
@@ -106,7 +107,8 @@ public class HandsMenuUI : MonoBehaviour
     }
     private void SetObjective(Objectives objective)
     {
-        _objectiveText.text = objective.Objective;
+        if(objective != null)
+            _objectiveText.text = objective.Objective;
     }
     private void SetLifeValue(int value)
     {
