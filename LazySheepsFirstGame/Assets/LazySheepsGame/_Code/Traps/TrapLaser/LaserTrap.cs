3d854db2e38dd20d429751990faa241553c3f08e
Dino <dino@gmail.com>
2024-02-20 16:23:00 -0600

    laser trap

---
+++ LazySheepsFirstGame/Assets/LazySheepsGame/_Code/Traps/TrapLaser/LaserTrap.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrap : MonoBehaviour
+{
+    
+    
+}
+
+public enum LaserTrapState
+{
+    Innactive,
+    Active,
+    Triggered
+}
